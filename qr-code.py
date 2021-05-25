67e03ec8b5246f934a526e75dfd0d137fd9b0b45
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2021-05-25 19:08:08 +0530

    Update qr-code.py
    Create functions for various tasks

--- qr-code.py
+++ qr-code.py
@@ -7,25 +7,56 @@ This is a Python Program to display QR Code from an input.
 __author__ = "Jugal Kishore"
 
 
-import qrcode
+try:
+    import qrcode
+except ImportError as e:
+    print("Importing of 'qrcode' library failed with error")
+    print("")
+    print(e)
+    exit(1)
 
-print("QR Code Generator")
-print("")
-print("Enter String Value: ")
-data = input()
-print("")
-print(f"Generating QR Code for: {data}")
-print("")
 
-# Create an instance of QRCode object
-qr = qrcode.QRCode(version=1, box_size=10, border=5)
+def generate_qrcode(message):
+    """
+    Generates a QR Code
+    :param message: Data to generate QR Code from.
+    """
 
-# Generate QR Code
-qr.add_data(data)
-qr.make(fit=True)
+    # Create an instance of QRCode object
+    qr = qrcode.QRCode(version=1, box_size=10, border=5)
 
-# Generate Image and Save it
-img = qr.make_image(fill="black", back_color="white")
-img.save("qrcode.png")
+    # Generate QR Code
+    qr.add_data(message)
+    qr.make(fit=True)
 
-print("Your QR Code was saved to 'qrcode.png' file.")
+    return qr
+
+
+def save_qrcode(qr_obj):
+    """
+    Generates a png picture and saves it.
+    :param qr_obj: QRCode object.
+    """
+
+    # Generate Image and Save it
+    img = qr_obj.make_image(fill="black", back_color="white")
+    img.save("qrcode.png")
+
+
+def main():
+    print("QR Code Generator")
+    print("")
+    print("Enter String Value: ")
+    data = input()
+    print("")
+    print(f"Generating QR Code for: {data}")
+    print("")
+
+    generated = generate_qrcode(data)
+    save_qrcode(generated)
+
+    print("Your QR Code was saved to 'qrcode.png' file.")
+
+
+if __name__ == "__main__":
+    main()
