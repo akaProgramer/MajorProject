5585e8dbb922973d4355fc32197c74bc6704cb81
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2021-05-25 18:48:18 +0530

    Update qr-code.py

--- qr-code.py
+++ qr-code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-This is Python Program to display QR Code from a string
+This is a Python Program to display QR Code from an input.
 """
 
 __author__ = "Jugal Kishore"
@@ -13,6 +13,9 @@ print("QR Code Generator")
 print("")
 print("Enter String Value: ")
 data = input()
+print("")
+print(f"Generating QR Code for: {data}")
+print("")
 
 # Create an instance of QRCode object
 qr = qrcode.QRCode(version=1, box_size=10, border=5)
@@ -24,3 +27,5 @@ qr.make(fit=True)
 # Generate Image and Save it
 img = qr.make_image(fill="black", back_color="white")
 img.save("qrcode.png")
+
+print("Your QR Code was saved to 'qrcode.png' file.")
