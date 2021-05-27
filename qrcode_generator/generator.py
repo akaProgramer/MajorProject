5dcaca87d35412f4c2d7316b7aca3cf2f5c9437e
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2021-05-27 19:43:50 +0530

    Update qrcode_generator/generator.py
    Make it more OOPS like as suggested by @akaProgramer
    
    Signed-off-by: crazyuploader <49350241+crazyuploader@users.noreply.github.com>

--- qrcode_generator/generator.py
+++ qrcode_generator/generator.py
@@ -16,37 +16,41 @@ except ImportError as e:
     exit(1)
 
 
-def generate_qrcode(message):
-    """
-    Generates a QR Code
-    :param message: Data to generate QR Code from.
-    """
+class Generator:
+    def __init__(self, data):
+        self.data = data
 
-    # Create an instance of QRCode object
-    qr = qrcode.QRCode(version=1, box_size=10, border=5)
+    def generate(self):
+        """
+        Generates a QR Code
+        :param message: Data to generate QR Code from.
+        """
 
-    # Generate QR Code
-    qr.add_data(message)
-    qr.make(fit=True)
+        # Create an instance of QRCode object
+        qr = qrcode.QRCode(version=1, box_size=10, border=5)
 
-    return qr
+        # Generate QR Code
+        qr.add_data(self.data)
+        qr.make(fit=True)
 
+        self.qr = qr
 
-def save_qrcode(qr_obj):
-    """
-    Generates a png picture and saves it.
-    :param qr_obj: QRCode object.
-    """
+    def save(self):
+        """
+        Generates a png picture and saves it.
+        :param qr_obj: QRCode object.
+        """
 
-    # Generate Image and Save it
-    img = qr_obj.make_image(fill="black", back_color="white")
-    img.save("qrcode.png")
+        # Generate Image and Save it
+        img = self.qr.make_image(fill="black", back_color="white")
+        img.save("qrcode.png")
 
 
 def main():
     """
     Main Function of the QR Code Generator Program.
     """
+
     print("QR Code Generator")
     print("")
     print("Enter String Value: ")
@@ -55,8 +59,9 @@ def main():
     print(f"Generating QR Code for: {data}")
     print("")
 
-    generated = generate_qrcode(data)
-    save_qrcode(generated)
+    generated = Generator(data)
+    generated.generate()
+    generated.save()
 
     print("Your QR Code was saved to 'qrcode.png' file.")
 
