d71b98f5d10fcddca1d28e4e206be6a957d08649
Akash Shiva <51360466+akaProgramer@users.noreply.github.com>
2021-07-08 20:58:30 +0530

    OOPS

--- OCR/OCR.py
+++ OCR/OCR.py
@@ -1,9 +1,16 @@
 import easyocr
 import cv2
 import numpy as np
-reader = easyocr.Reader(['en','hi'])
-results = reader.readtext("OCR/test.png")
-text= ''
-for result in results:
-    text += result[1] + " \n"
-print(text)
+
+class text_extracter:
+    def __init__(self):
+        reader = easyocr.Reader(['en','hi'])
+        self.results = reader.readtext("OCR/test.png")
+        self.text= ''
+    def extrated_text(self): 
+        for self.result in self.results:
+            self.text += self.result[1] + " \n"
+        print(self.text)
+
+obj = text_extracter()
+obj.extrated_text()
