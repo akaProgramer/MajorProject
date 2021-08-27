ae1a62f36cbc7398edc88ef9a92e2d37ab54905b
Jugal Kishore <me@devjugal.com>
2021-08-27 15:56:35 +0530

    Add PDF File Generator functions
    Update img_converter/converter.py
    
    Signed-off-by: Jugal Kishore <me@devjugal.com>

--- img_converter/converter.py
+++ img_converter/converter.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 """
-This is a Python Program which converts PNG/JPG to JPG/PNG
+This is a Python Program which converts PNG/JPG image(s) to JPG/PNG image(s).
+Also, it can generate PDF file from JPG/PNG image(s).
 """
 
 __author__ = "Ankit Mahor"
@@ -9,6 +10,7 @@ __author__ = "Ankit Mahor"
 
 # Importing the module(s)
 from PIL import Image
+from fpdf import FPDF
 import os
 import glob
 
@@ -22,8 +24,10 @@ else:
 
 class Convert:
     """
-    An image converter class which can convert a PNG, JPG to JPG, PNG interchangably.
+    An image converter class which can convert PNG, JPG image(s) to JPG, PNG image(s),
+    and vice versa. Also, it can convert JPG, PNG image(s) to PDF file(s).
     """
+
     def __init__(self, source_directory, target_directory):
         self.source_directory = source_directory
         self.target_directory = target_directory
@@ -40,12 +44,12 @@ class Convert:
 
     def png_to_jpg(self) -> None:
         """
-        This function converts a PNG image to a JPG image.
+        This function converts PNG image(s) to JPG image(s).
         """
 
         file_list = glob.glob(f"{self.source_directory}{SLASH}*.png")
         if not file_list:
-            print("No PNG files found in source directory: " + self.source_directory)
+            print("No PNG file(s) found in source directory: " + self.source_directory)
             print("Exiting...")
             exit(1)
         for file in file_list:
@@ -64,7 +68,7 @@ class Convert:
 
         file_list = glob.glob(f"{self.source_directory}{SLASH}*.jpg")
         if not file_list:
-            print("No JPG files found in source directory: " + self.source_directory)
+            print("No JPG file(s) found in source directory: " + self.source_directory)
             print("Exiting...")
             exit(1)
         for file in file_list:
@@ -76,12 +80,57 @@ class Convert:
             print("Converted: " + target_file)
             print("")
 
+    def jgp_to_pdf(self) -> None:
+        """
+        This function converts JPG image(s) to a PDF file.
+        """
+
+        file_list = glob.glob(f"{self.source_directory}{SLASH}*.jpg")
+        if not file_list:
+            print("No JPG file(s) found in source directory: " + self.source_directory)
+            print("Exiting...")
+            exit(1)
+
+        pdf = FPDF()
+        for file in file_list:
+            print("Adding file: " + file)
+            pdf.add_page()
+            pdf.image(file, 0, 0, 210, 297)
+        target_file = f"{self.target_directory}{SLASH}output.pdf"
+        pdf.output(target_file, "F")
+        print("")
+        print("Created PDF File -> " + target_file)
+
+    def png_to_pdf(self) -> None:
+        """
+        This function converts PNG image(s) to a PDF File.
+        """
+
+        file_list = glob.glob(f"{self.source_directory}{SLASH}*.png")
+        if not file_list:
+            print("No PNG file(s) found in source directory: " + self.source_directory)
+            print("Exiting...")
+            exit(1)
+
+        pdf = FPDF()
+        for file in file_list:
+            print("Adding file: " + file)
+            pdf.add_page()
+            pdf.image(file, 0, 0, 210, 297)
+        target_file = f"{self.target_directory}{SLASH}output.pdf"
+        pdf.output(target_file, "F")
+        print("")
+        print("Created PDF File -> " + target_file)
+
 
 def main():
     print("Image Converter")
     print("")
-    print(" 1. Convert PNG to JPG/JPEG")
-    print(" 2. Convert JPG/JPEG to PNG")
+    print(" 1. Convert PNG image(s) to JPG/JPEG image(s)")
+    print(" 2. Convert JPG/JPEG image(s) to PNG image(s)")
+    print(" 3. Convert JPG/JPEG image(s) to PDF File")
+    print(" 4. Convert PNG image(s) to PDF File")
+    print("")
     print(" Nothing (press enter) to exit!")
     print("")
     choice = input("Enter your choice: ")
@@ -107,7 +156,12 @@ def main():
     elif choice == "2":
         converter = Convert(source_directory, target_directory)
         converter.jpg_to_png()
-
+    elif choice == "3":
+        converter = Convert(source_directory, target_directory)
+        converter.jgp_to_pdf()
+    elif choice == "4":
+        converter = Convert(source_directory, target_directory)
+        converter.png_to_pdf()
 
 if __name__ == "__main__":
     main()
