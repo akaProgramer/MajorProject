0989c43fcdb9156d8d65f5080134e2f984c09990
Jugal Kishore <me@devjugal.com>
2021-08-27 14:38:02 +0530

    Remove Redundant Code, Handle directories with no images properly
    img_converter/converter.py
    
    Signed-off-by: Jugal Kishore <me@devjugal.com>

--- img_converter/converter.py
+++ img_converter/converter.py
@@ -22,17 +22,12 @@ else:
 
 class Convert:
     """
-    An image converter class which can convert a PNG, JPEG to JPEG, PNG interchangably.
+    An image converter class which can convert a PNG, JPG to JPG, PNG interchangably.
     """
     def __init__(self, source_directory, target_directory):
         self.source_directory = source_directory
         self.target_directory = target_directory
 
-    def png_to_jpg(self) -> None:
-        """
-        This function converts a PNG image to a JPEG image.
-        """
-
         if not os.path.exists(self.source_directory):
             print("Can't find source directory")
             print("Exiting...")
@@ -43,40 +38,43 @@ class Convert:
             print(f"Created target directory: {self.target_directory}")
             print("")
 
-        for file in glob.glob(f"{self.source_directory}{SLASH}*.png"):
-            if file.endswith(".png"):
-                print("Converting: " + file)
-                im = Image.open(file, "r")
-                rgb_image = im.convert("RGB")
-                target_file = self.target_directory + SLASH + file.split(SLASH)[-1].split(".")[0] + ".jpg"
-                rgb_image.save(target_file)
-                print("Converted: " + target_file)
-                print("")
+    def png_to_jpg(self) -> None:
+        """
+        This function converts a PNG image to a JPG image.
+        """
+
+        file_list = glob.glob(f"{self.source_directory}{SLASH}*.png")
+        if not file_list:
+            print("No PNG files found in source directory: " + self.source_directory)
+            print("Exiting...")
+            exit(1)
+        for file in file_list:
+            print("Converting: " + file)
+            im = Image.open(file, "r")
+            rgb_image = im.convert("RGB")
+            target_file = self.target_directory + SLASH + file.split(SLASH)[-1].split(".")[0] + ".jpg"
+            rgb_image.save(target_file)
+            print("Converted: " + target_file)
+            print("")
 
     def jpg_to_png(self) -> None:
         """
-        This function converts a JPEG image to a PNG image.
+        This function converts a JPG image to a PNG image.
         """
 
-        if not os.path.exists(self.source_directory):
-            print("Can't find source directory")
+        file_list = glob.glob(f"{self.source_directory}{SLASH}*.jpg")
+        if not file_list:
+            print("No JPG files found in source directory: " + self.source_directory)
             print("Exiting...")
             exit(1)
-        if not os.path.exists(self.target_directory):
-            print("Can't find target directory, creating...")
-            os.makedirs(self.target_directory)
-            print(f"Created target directory: {self.target_directory}")
+        for file in file_list:
+            print("Converting: " + file)
+            im = Image.open(file, "r")
+            rgb_image = im.convert("RGB")
+            target_file = self.target_directory + SLASH + file.split(SLASH)[-1].split(".")[0] + ".png"
+            rgb_image.save(target_file)
+            print("Converted: " + target_file)
             print("")
-
-        for file in glob.glob(f"{self.source_directory}{SLASH}*.jpg"):
-            if file.endswith(".jpg"):
-                print("Converting: " + file)
-                im = Image.open(file, "r")
-                rgb_image = im.convert("RGB")
-                target_file = self.target_directory + SLASH + file.split(SLASH)[-1].split(".")[0] + ".png"
-                rgb_image.save(target_file)
-                print("Converted: " + target_file)
-                print("")
 
 
 def main():
@@ -94,8 +92,14 @@ def main():
     print("")
     source_directory = str(input("Please enter the source directory: "))
     print("")
+    if not source_directory:
+        print("Exiting...")
+        exit(1)
     target_directory = str(input("Please enter the destination directory: "))
     print("")
+    if not target_directory:
+        print("Exiting...")
+        exit(1)
 
     if choice == "1":
         converter = Convert(source_directory, target_directory)
